b289da3da7a42b31d25acc0e73f4d5928c571da5
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-04-19 17:41:12 +0200

    fix side bar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,14 +1,26 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, input, InputSignal,ViewChild  } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClass } from 'primeng/styleclass';
+import { Drawer } from 'primeng/drawer';
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink , RouterLinkActive,DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
   islogged:InputSignal<boolean> = input.required();
+  @ViewChild('drawerRef') drawerRef!: Drawer;
+
+    closeCallback(e:any): void {
+        this.drawerRef.close(e);
+    }
+
+    visible: boolean = false;
 
 }
